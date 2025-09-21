83aac6e55d5c6ea563d82f36d4463200610bcd9a
Constantin Angheloiu <constantin.angheloiu@gmail.com>
2025-09-21 20:44:40 +0300

    fix hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -10,7 +10,12 @@ export default function Hero() {
   return (
     <section className="relative z-10 flex min-h-[100lvh] -mt-16 w-full">
       <div
-        className={`absolute -z-10 h-full min-h-full w-full overflow-hidden bg-cover bg-[url("/api/_plugin/image/optimize/${page.cover.path}?width=1080&height=960&fit=cover")]`}
+        className={`absolute -z-10 h-full min-h-full w-full overflow-hidden bg-cover bg-no-repeat`}
+        style={{
+          backgroundImage: `url("/api/_plugin/image/optimize/${encodeURIComponent(
+            page.cover.path,
+          )}?width=1080&height=960&fit=cover")`,
+        }}
       >
         {/*<Image
 			src={heroImage}
