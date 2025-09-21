89bda541715a88d8e4d31cc7d251f0deabd1847f
Constantin Angheloiu <constantin.angheloiu@gmail.com>
2025-09-21 14:31:27 +0300

    update config and cmds

--- bknd.config.ts
+++ bknd.config.ts
@@ -8,4 +8,9 @@
 import { withPlatformProxy } from "bknd/adapter/cloudflare/proxy";
 import config from "./config";
 
-export default withPlatformProxy(config);
+export default withPlatformProxy(config, {
+  useProxy: true,
+  proxyOptions: {
+    environment: process.env.CLOUDFLARE_ENV,
+  },
+});
