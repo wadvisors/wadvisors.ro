9a52f07940fc97a5dfc822ebc71abe27efde189f
Constantin Angheloiu <constantin.angheloiu@gmail.com>
2025-09-24 04:26:12 +0300

    move vars outside

--- config.ts
+++ config.ts
@@ -29,7 +29,8 @@ export default {
             issuer: "wadvisors",
             secret: env.SECRET,
           },
-          guard: { enabled: env.ENVIRONMENT !== "development" },
+          guard: { enabled: false },
+          // guard: { enabled: env.ENVIRONMENT !== "development" },
           roles: {
             EDITOR: {
               is_default: true,
