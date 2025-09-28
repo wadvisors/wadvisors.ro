e5f0d5347073cd0cb16c10709d88cbf64cbb456f
Constantin Angheloiu <constantin.angheloiu@gmail.com>
2025-09-29 00:05:21 +0300

    add intersection provider

---
+++ app/providers/ObserverProvider.tsx
@@ -0,0 +1,14 @@
+import { Observer } from "tailwindcss-intersect";
+import { useEffect } from "react";
+
+export default function ObserverProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    Observer.start();
+  }, []);
+
+  return <>{children}</>;
+}
