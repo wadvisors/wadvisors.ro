1ba2ae1233ac689722dcb44324db04706cb4bcf0
Constantin Angheloiu <constantin.angheloiu@gmail.com>
2025-10-25 22:05:39 +0300

    updates

--- app/loaders/featuredArticlesLoader.ts
+++ app/loaders/featuredArticlesLoader.ts
@@ -6,9 +6,12 @@ async function pressLoader(args: LoaderFunctionArgs) {
   const language = args.context.bknd.language;
   const api = args.context.bknd.apiUI;
 
+  const dateLimit = toBucharestDate();
+  console.log("toBucharestDate()", dateLimit);
+
   const { data: featuredArticles } = await api.data.readMany("articles", {
     select: ["id", "handle", "title_t", "tags", "publish_at"],
-    where: { status: "PUBLISHED", publish_at: { $lte: toBucharestDate() } },
+    where: { status: "PUBLISHED", publish_at: { $lte: dateLimit } },
     with: {
       cover: {
         select: ["path"],
