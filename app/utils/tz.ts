ece7737576c066cf32f9851d2543deff70a8a3ab
Constantin Angheloiu <constantin.angheloiu@gmail.com>
2025-10-25 22:14:02 +0300

    updates

--- app/utils/tz.ts
+++ app/utils/tz.ts
@@ -1,10 +1,11 @@
 import { datetimeStringUTC } from "bknd/utils";
 
-const TZ = "Europe/Bucharest";
+// const TZ = "Europe/Bucharest";
 
 export default function toBucharestDate(utcDate?: string) {
   const date = utcDate ? new Date(utcDate) : new Date();
-  const str = date.toLocaleString("en-US", { timeZone: TZ });
+  // const str = date.toLocaleString("en-US", { timeZone: TZ });
+  // const str = date.toLocaleString("en-US");
 
-  return datetimeStringUTC(str);
+  return datetimeStringUTC(date);
 }
